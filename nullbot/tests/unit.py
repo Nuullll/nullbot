4b811b008a8807a254278a21e60c2da2bf97a3ef
Nuullll <vfirst218@gmail.com>
2020-02-23 18:35:07 +0800

    Update tests.

--- nullbot/tests/unit.py
+++ nullbot/tests/unit.py
@@ -5,8 +5,8 @@ from nullbot.utils.helpers import multiline_msg_generator
 
 
 QQ_ID = 724463877
-GROUP_ID = 598880963
-GROUP_ID = 958127821
+GROUP_ID_200 = 598880963
+GROUP_ID_WEEKLY = 958127821
 
 
 async def tell_dad(msg):
@@ -30,5 +30,38 @@ async def test_db_refactor(session: CommandSession):
 
     dm = DataManager(group_id)
     
-    ok, snapshot = dm.get_and_save_user_summary(QQ_ID, 'nuullll', 'leetcode')
-    await session.send(snapshot.accepted)
+    ok, snapshot = await dm.get_and_save_user_summary(QQ_ID, 'nuullll', 'leetcode')
+    await session.send(str(snapshot.accepted))
+
+
+@on_command('do_db_refactor', permission=SUPERUSER)
+async def do_db_refactor(session: CommandSession):
+    group_id = 1234
+
+    dm = DataManager(group_id)
+
+    for doc in dm.collection.find({}):
+        qq_id = doc['qq_id']
+        if 'accounts' not in doc or len(doc['accounts']) == 0:
+            continue
+        
+        for key, data in doc['accounts'].items():
+            
+            for timestamp, fields in data.items():
+                dm.collection.update_one({
+                    'qq_id': qq_id
+                }, {
+                    '$push': {
+                        f"accounts.{key}.snapshots": {
+                            "timestamp": int(timestamp),
+                            "data": fields
+                        }
+                    }, 
+                    '$unset': {
+                        f"accounts.{key}.{timestamp}": ""
+                    }
+                })
+
+    for doc in dm.collection.find({}):
+        print(doc)
+    
