cc6d17436165427446b9f762908f1650cfb15f1f
Nuullll <vfirst218@gmail.com>
2020-02-23 19:01:40 +0800

    Fix typo.

--- nullbot/manager/commands.py
+++ nullbot/manager/commands.py
@@ -229,7 +229,7 @@ async def show_progress(session: CommandSession):
     for user_id, platform in accounts:
         snap = dm.load_latest_snapshot(qq_id, user_id, platform)
         lines.append(f"{user_id}@{platform}: ")
-        lines.extent(snap.lines)
+        lines.extend(snap.lines)
     
     for msg in multiline_msg_generator(lines):
         session.send(msg)
