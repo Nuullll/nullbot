978d319e101dd0bc942422c19f116d71849c9e60
Nuullll <vfirst218@gmail.com>
2020-03-07 11:31:36 +0800

    Add notice command.

--- nullbot/basic/commands.py
+++ nullbot/basic/commands.py
@@ -32,3 +32,18 @@ async def handle_ls(session: CommandSession):
                 break
     except Exception as e:
         print(type(e), e)
+
+
+@on_command('notice', permission=SUPERUSER)
+async def publish_notice(session: CommandSession):
+
+    if 'group_id' in session.ctx:
+        return
+    
+    msg = session.current_arg
+
+    data = await session.bot.get_group_list()
+    for g in data:
+        group_id = g['group_id']
+
+        await session.bot.send_msg_rate_limited(group_id=group_id, message=msg)
