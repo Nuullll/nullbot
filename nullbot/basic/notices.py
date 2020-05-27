e10683ce515e2dab17954ab0a21d74a9b5fa9852
Nuullll <vfirst218@gmail.com>
2020-05-27 15:37:38 +0800

    Change welcome message.

--- nullbot/basic/notices.py
+++ nullbot/basic/notices.py
@@ -4,4 +4,5 @@ from nonebot import on_notice, NoticeSession
 @on_notice('group_increase')
 async def test_handle_group_increase(session: NoticeSession):
     user_id = session.ctx['user_id']
-    await session.send('欢迎大佬' + '[CQ:at,qq={}]'.format(user_id))
+    await session.send(f'''欢迎大佬! [CQ:at,qq={user_id}]
+发送help，可查看闹闹支持的命令''')
